3f8919ca58f5beddcb387ac676dba2139dc8dfe0
jrp2014 <jrp@dial.pipex.com>
2019-03-09 00:04:18 +0000

    Add some benchmarks

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,7 +1,32 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
 
 import Gauge.Main
+import ThereAndBackAgain
 
+xs :: [Int]
+xs = [1 .. 10000]
+
+ys :: [Int]
+ys = [10001 .. 20000]
+
+lxs :: Int
+lxs = length xs
 
 main :: IO ()
-main = defaultMain [bench "const" (whnf const ())]
+main =
+  defaultMain
+    [ bench "convolutionT" (nf (convolutionT xs) ys)
+    , bench "convolutionC" (nf (convolutionC xs) ys)
+    , bench "convolutionD" (nf (convolutionD xs) ys)
+    , bench "cnv1" (nf cnv1 (xs, ys))
+    , bench "cnv2" (nf cnv2 (xs, ys))
+    , bench "cnv3" (nf cnv3 (xs, ys))
+    , bench "cnv3'" (nf (cnv3' xs) ys)
+    , bench "reverse" (nf reverse xs)
+    , bench "tabaRev" (nf tabaRev xs)
+    , bench "tabaRevOpt" (nf tabaRevOpt xs)
+--    , bench "catalan" (whnf catalan (10 :: Int))
+    , bench "cnvHalves" (nf cnvHalves (xs, lxs))
+    ]
--- src/ThereAndBackAgain.hs
+++ src/ThereAndBackAgain.hs
@@ -4,87 +4,82 @@ module ThereAndBackAgain where
 -- 2002
 convolutionT :: [a] -> [b] -> [(a, b)]
 convolutionT xs ys = zipT xs (reverse ys)
-  where
-    zipT [] [] = []
-    zipT (a:as) (b:bs) = (a, b) : zipT as bs
+ where
+  zipT []       []       = []
+  zipT (a : as) (b : bs) = (a, b) : zipT as bs
 
 convolutionC :: [a] -> [b] -> [(a, b)]
 convolutionC xs ys = walk xs fst
-  where
-    walk [] k = k ([], ys)
-    walk (a:as) k = walk as (\(zs, b:bs) -> k ((a, b) : zs, bs))
+ where
+  walk []       k = k ([], ys)
+  walk (a : as) k = walk as (\(zs, b : bs) -> k ((a, b) : zs, bs))
 
 convolutionD :: [a] -> [b] -> [(a, b)]
 convolutionD xs ys = fst . walk $ xs
-  where
-    walk [] = ([], ys)
-    walk (x:xs) =
-      let (zs, y:ys) = walk xs
-       in ((x, y) : zs, ys)
+ where
+  walk []       = ([], ys)
+  walk (x : xs) = let (zs, y : ys) = walk xs in ((x, y) : zs, ys)
 
 -- 2005
 cnv1 :: ([a], [b]) -> [(a, b)]
 cnv1 (xs, ys) = walk (xs, [])
-  where
-    walk ([], a) = continue (a, ys, [])
-    walk (x:xs, a) = walk (xs, x : a)
-    continue ([], [], r) = r
-    continue (x:a, y:ys, r) = continue (a, ys, (x, y) : r)
+ where
+  walk ([]    , a) = continue (a, ys, [])
+  walk (x : xs, a) = walk (xs, x : a)
+
+  continue ([]   , []    , r) = r
+  continue (x : a, y : ys, r) = continue (a, ys, (x, y) : r)
 
 cnv2 :: ([a], [b]) -> [(a, b)]
 cnv2 (xs, ys) = walk (xs, \([], r) -> r)
-  where
-    walk ([], k) = k (ys, [])
-    walk (x:xs, k) = walk (xs, \(y:ys, r) -> k (ys, (x, y) : r))
+ where
+  walk ([]    , k) = k (ys, [])
+  walk (x : xs, k) = walk (xs, \(y : ys, r) -> k (ys, (x, y) : r))
 
 cnv3 :: ([a], [b]) -> [(a, b)]
 cnv3 (xs, ys) = snd . walk $ xs
-  where
-    walk [] = (ys, [])
-    walk (x:xs) =
-      let (y:ys, r) = walk xs
-       in (ys, (x, y) : r)
+ where
+  walk []       = (ys, [])
+  walk (x : xs) = let (y : ys, r) = walk xs in (ys, (x, y) : r)
+
+cnv3' :: [a] -> [b] -> [(a, b)]
+cnv3' xs ys = fst . foldr (\x (r, a : as) -> ((x, a) : r, as)) ([], ys) $ xs
 
 tabaRev :: [a] -> [a]
 tabaRev xs = snd . walk $ xs
-  where
-    walk [] = (xs, [])
-    walk (_:xs) =
-      let (y:ys, r) = walk xs
-       in (ys, y : r)
+ where
+  walk []       = (xs, [])
+  walk (_ : xs) = let (y : ys, r) = walk xs in (ys, y : r)
 
 tabaRevOpt :: [a] -> [a]
 tabaRevOpt xs = walkReturn (xs, [])
-  where
-    walkReturn ([], r) = r
-    walkReturn (y:ys, r) = walkReturn (ys, y : r)
+ where
+  walkReturn ([]    , r) = r
+  walkReturn (y : ys, r) = walkReturn (ys, y : r)
 
 suffixes :: ([a], [b]) -> [[(a, b)]]
-suffixes ([], []) = []
-suffixes xy@(_:xs, _:ys) = cnv3 xy : suffixes (xs, ys)
+suffixes (   []    , []    ) = []
+suffixes xy@(_ : xs, _ : ys) = cnv3 xy : suffixes (xs, ys)
 
 prefixes :: ([a], [b]) -> [[(a, b)]]
 prefixes (xs, ys) = walk (xs, snd)
-  where
-    walk ([], k) = [k (ys, [])]
-    walk (x:xs, k) = k (ys, []) : walk (xs, \(y:ys, r) -> k (ys, (x, y) : r))
+ where
+  walk ([]    , k) = [k (ys, [])]
+  walk (x : xs, k) = k (ys, []) : walk (xs, \(y : ys, r) -> k (ys, (x, y) : r))
 
 catalan :: (Ord a, Num a) => a -> a
 catalan m = iter (1, [1])
-  where
-    cat a = snd . walk $ a
-      where
-        walk [] = (a, 0)
-        walk (n:ns) =
-          let (n':ns', r) = walk ns
-           in (ns', r + (n * n'))
-    iter (i, a@(b:bs))
-      | i > m = b
-      | otherwise = iter (i + 1, cat a : a)
+ where
+  cat a = snd . walk $ a
+   where
+    walk []       = (a, 0)
+    walk (n : ns) = let (n' : ns', r) = walk ns in (ns', r + (n * n'))
 
+  iter (i, a@(b : bs)) | i > m     = b
+                       | otherwise = iter (i + 1, cat a : a)
+
+cnvHalves :: (Eq a, Num a) => ([b], a) -> [(b, b)]
 cnvHalves (xs, n) = snd . walk $ (n, xs)
-  where
-    walk (0, xs) = (xs, [])
-    walk (n, x:xs) =
-      let (y:ys, r) = walk (n - 2, xs)
-       in (ys, (x, y) : r)
+ where
+  walk (0, xs    ) = (xs, [])
+  walk (n, x : xs) = let (y : ys, r) = walk (n - 2, xs) in (ys, (x, y) : r)
